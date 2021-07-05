86cc959b84687ec103845f1ff0eb5edbf32ca618
Cauã <cauakathdev@gmail.com>
2021-07-05 11:47:51 -0300

    Inicio do projeto

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #c7c5bf;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 20px 40px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
